889bc4205fb32c6e6bd9f55a83806408a4b68ae5
zk <136334807@qq.com>
2024-03-25 13:43:56 +0800

    反射

--- src/HelloWorld.go
+++ src/HelloWorld.go
@@ -303,23 +303,38 @@ func main() {
 	/*var a int64 = 3
 	reflectSetValue2(&a)*/
 
-	var a *int
-	//指针为空那么就返回true 有值返回false
-	fmt.Println("var a *int IsNil:", reflect.ValueOf(a).IsNil())
-	//参数无效返回false 有效返回true
-	fmt.Println("nil IsValid:", reflect.ValueOf(nil).IsValid())
+	/*	var a *int
+		//指针为空那么就返回true 有值返回false
+		fmt.Println("var a *int IsNil:", reflect.ValueOf(a).IsNil())
+		//参数无效返回false 有效返回true
+		fmt.Println("nil IsValid:", reflect.ValueOf(nil).IsValid())
 
-	// 实例化一个匿名结构体
-	b := struct{}{}
-	// 尝试从结构体中查找"abc"字段
-	fmt.Println("不存在的结构体成员:", reflect.ValueOf(b).FieldByName("abc").IsValid())
-	// 尝试从结构体中查找"abc"方法
-	fmt.Println("不存在的结构体方法:", reflect.ValueOf(b).MethodByName("abc").IsValid())
-	// map
-	c := map[string]int{}
-	// 尝试从map中查找一个不存在的键
-	fmt.Println("map中不存在的键：", reflect.ValueOf(c).MapIndex(reflect.ValueOf("娜扎")).IsValid())
+		reflect.TypeOf(a)
 
+		// 实例化一个匿名结构体
+		b := struct{}{}
+		// 尝试从结构体中查找"abc"字段
+		fmt.Println("不存在的结构体成员:", reflect.ValueOf(b).FieldByName("abc").IsValid())
+		// 尝试从结构体中查找"abc"方法
+		fmt.Println("不存在的结构体方法:", reflect.ValueOf(b).MethodByName("abc").IsValid())
+		// map
+		c := map[string]int{}
+		// 尝试从map中查找一个不存在的键
+		fmt.Println("map中不存在的键：", reflect.ValueOf(c).MapIndex(reflect.ValueOf("娜扎")).IsValid())*/
+	s := &School{
+		name: "ja",
+		person: Person{
+			name: "zhangsan",
+			age:  42,
+		},
+	}
+	size := reflect.TypeOf(*s).NumField()
+	fmt.Println("字段长 :", size)
+}
+
+type School struct {
+	name   string
+	person Person
 }
 
 func reflectSetValue1(x interface{}) {
